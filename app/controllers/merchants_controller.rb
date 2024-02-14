41bfbc4c7ba2c5a9567a06e734ca6743a8d946df
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-14 15:53:29 -0700

    added poro for merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,7 +3,9 @@ class MerchantsController < ApplicationController
     conn = Faraday.new(url: "http://localhost:3000")
     response = conn.get("/api/v1/merchants")
     json = JSON.parse(response.body, symbolize_names: true)
-    @merchants = json[:data]
+    @merchants = json[:data].map do |merchant_data|
+      Merchant.new(merchant_data)
+    end
   end
 
   def show
